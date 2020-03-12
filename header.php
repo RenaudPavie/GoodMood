fe929747287edd0dc3992f7cbf1a9690a5acf1af
Unknown <renaud.hpavie@gmail.com>
2020-03-12 14:00:41 +0100

    Toujours de l'initialisation

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>GoodMood</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php bloginfo("stylesheet_directory") ?>/assets/css/master.css">
+  </head>
+  <body>
+
+    <!-- HEADER -->
+    <header class="container">
+      <div class="row">
+
+       
+
+      </div>
+    </header>
