d9fd97898bc213b34be6345c5be4c2f6c19675c0
DeborahSoude <37223534+DeborahSoude@users.noreply.github.com>
2020-03-12 17:09:58 +0100

    Commence les articles

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php get_header() ?>
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<div class="page">
+      
+      <!-- Banner -->
+      <div class="banner">
+        <div class="banner__content">
+          <h1><?php the_title() ?></h1>
+        </div>
+      </div>
+
+      <!-- Container page -->
+      <div class="container-page container">
+        <div class="row">
+
+          <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+            <?php the_content() ?>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+<?php endwhile; endif; ?>
+
 <?php get_footer() ?>
