ac5be22f7d0b518c1dd361a5422099bbdf6dfe8e
DeborahSoude <37223534+DeborahSoude@users.noreply.github.com>
2020-03-13 08:31:03 +0100

    Rend dynamique les articles

--- actualites.php
+++ actualites.php
@@ -14,27 +14,21 @@ get_header(); ?>
                 $recentPosts = new WP_Query();
                 $recentPosts->query('showposts=5');
             ?>
-            <?php while ($recentPosts->have_posts()) : $recentPosts->the_post(); ?>
+            <?php $args = array( 'numberposts' => 5, 'order'=> 'ASC', 'orderby' => 'title' );
+                $postslist = get_posts( $args );
+                foreach ($postslist as $post) :  setup_postdata($post); ?>
             <div class="article">
-                <p class="category">MUSIQUE</p>
-                <h3>POURQUOI ON AIME ALOÏSE SAUVAGE ?</h3>
-                <p>
-                Fine silhouette ultra tonique, carré brun, regard de braise, traits affirmés, beaucoup 
-                de jolies chaussettes … Le charisme est certain chez Aloïse Sauvage, et sa simplicité 
-                non feinte. Dès qu’il s’agit de mondanités, on devine de la retenue, voire de la méfiance 
-                chez celle qui s’entoure d’une garde rapprochée, tant familiale qu’amicale, à laquelle elle 
-                est fidèle depuis des années… Le tout pour un talent qui ne cesse de se façonner depuis sa 
-                naissance.
-                </p>
+                <p class="category" style="pointer-events: none !important;"><?php the_category($separator = ", "); ?></p>
+                <h3><?php the_title(); ?></h3>
+                <p><?php the_excerpt(); ?></p>
                 <div class="know-more">
-                    <p>EN SAVOIR PLUS</p>
+                <a href="<?php the_permalink() ?>"><p>EN SAVOIR PLUS</p></a>
                 </div>
                 <div class="thumbnail">
                     <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/photo.jpg" alt="">
                 </div>
             </div>
-                <li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></li>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
 
     </div>
 </div>
