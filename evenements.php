a30ba79a430ea5f74737390fee50ef95ab5f297e
Unknown <renaud.hpavie@gmail.com>
2020-03-13 10:13:12 +0100

    Création + style evenements

--- evenements.php
+++ evenements.php
@@ -1,17 +1,17 @@
 <?php /* Template Name: Évènements */ ?>
 
 <?php get_header(); ?>
-<div class="page">
+<div class="page container">
       
       <!-- Banner -->
       <div class="banner">
         <div class="banner__content">
-          <h1><?php wp_title() ?></h1>
+          <h1 class="mainTitlePerPage"><?php wp_title("", true, "") ?></h1>
         </div>
       </div>
 
       <!-- Container page -->
-      <div class="container-page container">
+      <!-- <div class="container-page container">
         <div class="row">
 
           <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
@@ -63,7 +63,51 @@
             </div>
           </div>
         </div>
+      </div> -->
+      <div class="eventWrapper row">
+        <div class="col-12 col-md-6 col-lg-4 eventBox">
+          <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/photo.jpg" alt="">
+          <h2>NEKFEU - 106</h2>  
+          <p>VENDREDI 17 JANVIER 2020 - 20h00</p>          
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 eventBox">
+          <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/photo.jpg" alt="">
+          <h2>NEKFEU - 106</h2>  
+          <p>VENDREDI 17 JANVIER 2020 - 20h00</p>          
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 eventBox">
+          <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/photo.jpg" alt="">
+          <h2>NEKFEU - 106</h2>  
+          <p>VENDREDI 17 JANVIER 2020 - 20h00</p>          
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 eventBox">
+          <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/photo.jpg" alt="">
+          <h2>NEKFEU - 106</h2>  
+          <p>VENDREDI 17 JANVIER 2020 - 20h00</p>          
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 eventBox">
+          <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/photo.jpg" alt="">
+          <h2>NEKFEU - 106</h2>  
+          <p>VENDREDI 17 JANVIER 2020 - 20h00</p>          
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 eventBox">
+          <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/photo.jpg" alt="">
+          <h2>NEKFEU - 106</h2>  
+          <p>VENDREDI 17 JANVIER 2020 - 20h00</p>          
+        </div>
+        
       </div>
-
+      </div>
+    </div>
+    <div class="bgEventBeforeFooter container-fluid">
+      <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/crowd.png" alt="Image de foule">
     </div>
     <?php get_footer() ?>
+
+
+
+<style>
+.mainTitlePerPage::before {
+    content:'<?php wp_title("", true, ""); ?>';
+}
+</style>
