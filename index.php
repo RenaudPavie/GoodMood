116ddde6ca4398ca1800b90d176b9efefcc91c86
Unknown <renaud.hpavie@gmail.com>
2020-03-13 13:50:00 +0100

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <?php get_header() ?>
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-<div class="page">
-      
+<div class="page container">
+      <div>
+        
+      </div>
       <!-- Banner -->
       <div class="banner">
         <div class="banner__content">
-          <h1><?php the_title() ?></h1>
+          <h1>DÉCOUVREZ NOS DERNIERS ARTICLES</h1>
         </div>
       </div>
 
       <!-- Container page -->
       <div class="container-page container">
         <div class="row">
+        <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
           <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
             <?php the_content() ?>
           </div>
+          <?php endwhile; endif; ?>
 
         </div>
       </div>
 
     </div>
-<?php endwhile; endif; ?>
 
 <?php get_footer() ?>
+
+<style>
+.mainTitlePerPage::before {
+    content:'DÉCOUVREZ NOS DERNIERS ARTICLES';
+}
+</style>
+
--- single-evenements.php
+++ single-evenements.php
@@ -1,39 +1,46 @@
 <?php get_header() ?>
     <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-    <div class="single">
-      
-      <!-- Banner -->
-      <div class="banner">
-        <div class="banner__content">
-          <div class="container">
-            <div class="row">
-              <div class="col-4">
-              <?php the_post_thumbnail() ?>          
-              </div>
-              <div class="col-8">
-                <h1><?php the_title() ?></h1>
-                <p>Publié le <?php the_date() ?> par <?php the_author() ?></p>
+    <div class="single page">
+      <div class="container-fluid eventBanniere">
+        <img src="<?php bloginfo("stylesheet_directory") ?>/assets/img/eventBanniere.jpg" alt="">
+      </div>
 
 
-                <!-- On affiche les taxonomies gràce à la fonction the_terms() -->
-              <?php the_terms( $post->ID, 'lieux', 'Lieu : ' ); ?> <br>
-              <?php the_terms( $post->ID, 'dates', 'Date : ' );?> <br>
-              <?php the_terms( $post->ID, 'heures', 'Heure : ' ); ?><br>
+      <div class="singleEventContent">
+        <!-- Banner -->
+        <div class="banner singleEvent">
+          <div class="banner__content">
+            <div class="container">
+              <div class="row">
+                <div class="col-6 eventImg">
+                <?php the_post_thumbnail() ?>          
+                </div>
+                <div class="col-6">
+                  <h1><?php the_title() ?></h1>
+                  <!-- <p>Publié le <?php the_date() ?> par <?php the_author() ?></p> -->
+
+
+                  <!-- On affiche les taxonomies gràce à la fonction the_terms() -->
+                <?php the_terms( $post->ID, 'lieux', 'Lieu : ' ); ?> <br>
+                <?php the_terms( $post->ID, 'dates', 'Date : ' );?> <br>
+                <?php the_terms( $post->ID, 'heures', 'Heure : ' ); ?><br>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <!-- Container page -->
-      <div class="container-page container">
-        <div class="row">
+        <!-- Container page -->
+        <div class="container-page container">
+          <div class="row">
 
-          <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
-          <?php the_content() ?>
+            <div class="col-12">
+            <?php the_content() ?>
+            </div>
+
           </div>
-
         </div>
+
       </div>
 
     </div>
